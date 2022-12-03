ab1e1104eec1bda7e2becad6635dec3d68f47640
pedro <pedro@T440p>
2022-12-02 20:52:59 -0600

    Commit 5 pedro Mi circuito prros

--- src/proyectograf/Circuito1.java
+++ src/proyectograf/Circuito1.java
@@ -15,6 +15,7 @@ import tiburcio.lib2D.s2D;
  * @author reyes
  */
 public class Circuito1 extends JPanel {
+    Metodos m = new Metodos();
     //Coordenadas del PCB verde
     public int PantallaRect1X[]={380,650,650,380};
     public int PantallaRect1Y[]={240,240,350,350};
@@ -25,7 +26,7 @@ public class Circuito1 extends JPanel {
     public int PantallaRect3X[]={400,630,630,400};
     public int PantallaRect3Y[]={270,270,330,330};
     
-    public int coordPINLCDX=390;
+    public int coordPINLCDX=405;
     public int coordPINLCDY=243;
     public int coordsPINLCDX[]=new int[16];
     
@@ -36,14 +37,139 @@ public class Circuito1 extends JPanel {
     }
 
     public void paint(Graphics g) {
-        Metodos m = new Metodos();
+        
         g.setColor(Color.white);
         g.fillRect(0, 0, 700, 700);
         m.Arduino(g);
         m.Proto(g);
+        dibujarLCD(g);
+        dibujarPotenciometro(g);
+        dibujarResistencias(g, m.coordProto2X[0][15], m.coordProto2Y[0][15]);
+        dibujarCables(g);
+    }
+    
+    public void dibujarCables(Graphics g){
+        //PUENTE TIERRA y FASE
+        g.setColor(Color.black);
+        g.fillRect(m.coordProto1X[0][25], m.coordProto1Y[0][25], 5, 120);
+        g.setColor(Color.red);
+        g.fillRect(m.coordProto1X[1][26], m.coordProto1Y[1][26], 5, 125);
         
+        //Resistencia
+        g.setColor(Color.decode("#9c6028"));
+        g.fillRect(m.coordProto1X[1][15], m.coordProto1Y[1][15], 5, 27);
+        g.fillRect(m.coordProto2X[2][15], m.coordProto2Y[2][15]+5, 5, 100);
+        
+        //Potenciometro
+        g.setColor(Color.red);
+        g.fillRect(m.coordProto2X[3][1], m.coordProto2Y[3][1], 5, 25);
+        
+        g.setColor(Color.black);
+        g.fillRect(m.coordProto2X[3][3], m.coordProto2Y[3][3], 5, 35);
+        
+        //PIN 2
+        g.setColor(Color.decode("#68289c"));
+        g.fillRect(m.coordProto1X[0][14], m.coordProto1Y[0][14]-35, 5, 220);
+        g.fillRect(m.coordPines[15][0], m.coordPines[15][1]-17, 5, 22);
+        g.fillRect(m.coordPines[15][0], m.coordPines[15][1]-17, 265, 5);
+        
+        //PIN 3
+        g.setColor(Color.decode("#0d70b8"));
+        g.fillRect(m.coordProto1X[0][13], m.coordProto1Y[0][13]-42, 5, 227);
+        g.fillRect(m.coordPines[14][0], m.coordPines[14][1]-24, 5, 29);
+        g.fillRect(m.coordPines[14][0], m.coordPines[14][1]-24, 265, 5);
+        
+        //PIN 4
+        g.setColor(Color.decode("#5cb6f7"));
+        g.fillRect(m.coordProto1X[0][12], m.coordProto1Y[0][12]-49, 5, 234);
+        g.fillRect(m.coordPines[13][0], m.coordPines[13][1]-31, 5, 36);
+        g.fillRect(m.coordPines[13][0], m.coordPines[13][1]-31, 265, 5);
+        
+        //PIN 5
+        g.setColor(Color.decode("#33a345"));
+        g.fillRect(m.coordProto1X[0][11], m.coordProto1Y[0][11]-56, 5, 241);
+        g.fillRect(m.coordPines[12][0], m.coordPines[12][1]-38, 5, 43);
+        g.fillRect(m.coordPines[12][0], m.coordPines[12][1]-38, 265, 5);
+        
+        //PIN 12
+        g.setColor(Color.yellow);
+        g.fillRect(m.coordProto1X[0][8], m.coordProto1Y[0][8]-63, 5, 248);
+        g.fillRect(m.coordPines[5][0], m.coordPines[5][1]-45, 5, 50);
+        g.fillRect(m.coordPines[5][0], m.coordPines[5][1]-45, 310, 5);
+        
+        //PIN 11
+        g.setColor(Color.decode("#fa7e02"));
+        g.fillRect(m.coordProto1X[0][6], m.coordProto1Y[0][6]-70, 5, 255);
+        g.fillRect(m.coordPines[4][0], m.coordPines[4][1]-52, 5, 57);
+        g.fillRect(m.coordPines[4][0], m.coordPines[4][1]-52, 279, 5);
+        
+        //ALIMENTACION
+        g.setColor(Color.black);
+        g.fillRect(m.coordProto3X[1][0]-25, m.coordProto3Y[1][0], 30, 5);
+        g.fillRect(m.coordProto3X[1][0]-25, m.coordProto3Y[1][0], 5, 65);
+        
+        g.setColor(Color.red);
+        g.fillRect(m.coordProto3X[0][0]-35, m.coordProto3Y[0][0]-5, 40, 5);
+        g.fillRect(m.coordProto3X[0][0]-35, m.coordProto3Y[0][0]-5, 5, 60);
+        
+        //Bateria
+        g.setColor(Color.decode("#cccccc"));
+        g.fillRect(m.coordProto3X[1][0]-40, m.coordProto3Y[1][0]+60, 40, 80);
+        
+        g.setColor(Color.decode("#bd8759"));
+        g.fillRect(m.coordProto3X[1][0]-40, m.coordProto3Y[1][0]+65, 40, 80);
+        
+        //EXTRA CABLE
+        g.setColor(Color.black);
+        g.fillRect(m.coordProto3X[1][7], m.coordProto3Y[1][7], 5, 65);
+        
+        g.fillRect(m.coordProto3X[1][16], m.coordProto3Y[1][16], 5, 65);
+        
+        g.setColor(Color.red);
+        g.fillRect(m.coordProto3X[0][4], m.coordProto3Y[0][4]-5, 5, 50);
+        
+        g.setColor(Color.magenta);
+        g.fillRect(m.coordProto2X[3][2], m.coordProto2Y[3][2], 35, 5);
+        g.fillRect(m.coordProto2X[3][5], m.coordProto2Y[3][5], 5, 85);
+    }   
+    public void dibujarResistencias(Graphics g,int x,int y){
+        //Patas
+        g.setColor(Color.gray);
+        g.fillRect(x, y, 5, 45);
+        //Cuerpo
+        g.setColor(Color.orange);
+        g.fillRect(x-3, y+10, 10, 25);
+        
+        //RAYAS
+        g.setColor(Color.red);
+        g.fillRect(x-3, y+13, 10, 3);
+        
+        g.setColor(Color.red);
+        g.fillRect(x-3, y+19, 10, 3);
+        
+        g.setColor(Color.MAGENTA);
+        g.fillRect(x-3, y+26, 10, 3);
+        
+    }
+    
+    public void dibujarPotenciometro(Graphics g){
+        g.setColor(Color.gray);
+        g.fillRect(m.coordProto2X[2][1], m.coordProto2Y[2][1], 5, 20);
+        g.fillRect(m.coordProto2X[2][2], m.coordProto2Y[2][2], 5, 20);
+        g.fillRect(m.coordProto2X[2][3], m.coordProto2Y[2][3], 5, 20);
+        
+        g.setColor(Color.decode("#2e5878"));
+        g.fillOval(m.coordProto2X[1][1]-10, m.coordProto2Y[1][1]-10, 40, 40);
+        g.setColor(Color.decode("#5a9ed1"));
+        g.fillOval(m.coordProto2X[1][1]-5, m.coordProto2Y[1][1]-5, 30, 30);
+        g.setColor(Color.black);
+        g.fillRect(m.coordProto2X[1][1]+8, m.coordProto2Y[1][1]+10, 4, 20);
+        
+        
+    }
+    public void dibujarLCD(Graphics g){
         //DIBUJAR PANTALLA LCD
-        g.setColor(Color.green);
+        g.setColor(Color.decode("#3bad61"));
         g.fillPolygon(PantallaRect1X,PantallaRect1Y,4);
         
         g.setColor(Color.DARK_GRAY);
@@ -58,7 +184,6 @@ public class Circuito1 extends JPanel {
             coordsPINLCDX[i]=coordPINLCDX;
             coordPINLCDX=coordPINLCDX+10;
         }
-        
     }
 
 }
